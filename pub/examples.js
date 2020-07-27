201112d74ac9cc13f0ba9f1771f5acf789c7dce6
Khizr Khan <khizr.khan@mail.utoronto.ca>
2020-07-26 20:24:11 -0400

    alpha release complete

--- pub/examples.js
+++ pub/examples.js
@@ -4,8 +4,58 @@ const log = console.log
 log('Loading DOM Content')
 
 
+const t1 = new PlugYourself()
+t1.setIconTheme("pastel")
+t1.setColour("#FFF8DC")
+t1.setIconSize(48)
+t1.addButton('whatsapp', 'https://www.whatsapp.com/')
+t1.addButton('skype', 'https://www.skype.com/en/')
+t1.addButton('twitter', 'https://twitter.com/UofT')
+t1.addButton('messenger', 'https://www.facebook.com/universitytoronto')
+
+const t2 = new PlugYourself()
+t2.setIconTheme("pastel")
+t2.setColour("#FFF8DC")
+t2.setIconSize(48)
+t2.addButton('youtube', 'https://www.youtube.com/user/universitytoronto')
+t2.addButton('instagram', 'https://www.instagram.com/uoft/?hl=en')
+t2.addButton('twitter', 'https://twitter.com/UofT')
+
+const t3 = new PlugYourself()
+t3.setIconTheme("pastel")
+t3.setColour("#FFC0CB")
+t3.setIconSize(48)
+t3.addButton('youtube', 'https://www.youtube.com/user/universitytoronto')
+t3.addButton('instagram', 'https://www.instagram.com/uoft/?hl=en')
+t3.addButton('twitter', 'https://twitter.com/UofT')
+
+const t4 = new PlugYourself()
+t4.setIconTheme("pastel")
+t4.setColour("#FFC0CB")
+t4.setIconSize(60)
+t4.addButton('youtube', 'https://www.youtube.com/user/universitytoronto')
+t4.addButton('instagram', 'https://www.instagram.com/uoft/?hl=en')
+t4.addButton('twitter', 'https://twitter.com/UofT')
 
 
-const cg = new PlugYourself()
-cg.addButton('https://q.utoronto.ca/courses/159041/pages/lectures')
+
+const t5 = new PlugYourself()
+t5.setIconTheme("handDrawn")
+t5.setColour("#FFC0CB")
+t5.setIconSize(48)
+t5.addButton('youtube', 'https://www.youtube.com/user/universitytoronto')
+t5.addButton('instagram', 'https://www.instagram.com/uoft/?hl=en')
+t5.addButton('twitter', 'https://twitter.com/UofT')
+
+const t6 = new PlugYourself()
+t6.setIconTheme("material")
+t6.setColour("#FFC0CB")
+t6.setIconSize(48)
+t6.addButton('youtube', 'https://www.youtube.com/user/universitytoronto')
+t6.addButton('instagram', 'https://www.instagram.com/uoft/?hl=en')
+t6.addButton('twitter', 'https://twitter.com/UofT')
+
+
+
+
 //const cg2 = new CircleGenerator() // will have its own circles seperate from cg, but the same prototype reference
--- pub/plug_yourself.js
+++ pub/plug_yourself.js
@@ -1,76 +1,116 @@
 /* JS Library */
 "use strict";
-console.log('Js libraries')
-
-// Let's make a small Jquery clone.
-// one way to make a library is to write a function
-// that returns an object with the library functions, important
-// values, etc.
-
-function $$$(selector) {
-
-	const _self = {}
-	_self.selector = selector
-	_self.element = document.querySelector(selector) 
-
-	_self.text = function() {
-		return _self.element.innerText
-	}
-
-	_self.addClass = function(className) {
-		if (!_self.element.classList.contains(className)) {
-			_self.element.classList.add(className)
-		}
-	}
-
-	_self.attr = function(name, value) {
-		if (!value) {
-			return _self.element.getAttribute(name)
-		} else {
-			_self.element.setAttribute(name, value)
-		}
-	}
-
-	return _self
-}
-
-///////
-// Different way of creating library: creating an object constructor and then
-// adding to its prototype.
-
-// A Circle Generator Library
+console.log('creating js library')
 
 function PlugYourself() {
-	this.bar = document.createElement('div')
-	this.bar.style = 'display: block;'
-	document.querySelector('body').append(this.bar)
-	// this..
-	// this.. (any values you need for each 'instance' of this library)
+	this.bar = this.createBar()
+	this.buttons = []
+	this.theme = "handDrawn"
+	this.iconSize = 20
+	//this.title = this.createTitle()
 }
 
-// Added common funcionality to the prototype property of the constructor.
 PlugYourself.prototype = {
 
-	addButton: function(link) {
+	createBar: function() {
+		const bar = document.createElement('div')
+		bar.style = 'display: inline-block; padding: 10px; margin: 10px; border: 3px solid;'
+		document.querySelector('body').append(bar)
+		return bar
+	},
+
+	// createTitle: function() {
+	// 	const title_div = document.createElement('div')
+	// 	const title_text = document.createElement('h3')
+	// 	//title_text.appendChild(document.createTextNode("Check out my links below"))
+	// 	title_text.style = "display: inline-block"
+	// 	title_div.appendChild(title_text)
+	// 	this.bar.appendChild(title_div)
+	// 	return title_div
+	// },
+
+	setColour: function(colour) {
+		this.bar.style.backgroundColor = colour
+	},
+
+	addButton: function(type, link) {
 		var a = document.createElement('a');  
-        // Create the text node for anchor element. 
-    	//var link = document.createTextNode("This is link"); 
-        //a.appendChild(link);  
-        //a.title = "This is Link";  
         a.href = link;  
-		a.style='background-color: red; color: white; padding: 5px; text-decoration: none; text-transform: uppercase;'
+		a.style='color: white; padding: 5px; text-decoration: none; text-transform: uppercase;'
 		
 		const image = document.createElement('img')
-		image.src = "https://img.icons8.com/plasticine/100/000000/adobe-acrobat.png"
+		image.src = this.getImageLink(type)
+		image.style.width = self.IconSize + "px"
+		image.style.height = self.IconSize + "px"
 		a.appendChild(image)
-        // Append the anchor element to the body. 
 		this.bar.appendChild(a);  
 		
 	},
 
-	changeCirclesColor: function() {
-		for (let i = 0; i < this.circles.length; i++) {
-			this.circles[i].style.backgroundColor = 'darkmagenta'
+	setIconTheme: function(theme) {
+		self.theme = theme
+		
+	},
+
+	setIconSize: function(size) {
+		self.IconSize = size
+		
+	},
+
+	getImageLink: function(type) {
+		if (type === "youtube" && self.theme === "handDrawn"){
+			return "https://img.icons8.com/plasticine/100/000000/youtube-play.png"
+		}
+		else if (type === "instagram" && self.theme === "handDrawn"){
+			return "https://img.icons8.com/plasticine/100/000000/instagram-new.png"
+		}
+		else if (type === "twitter" && self.theme === "handDrawn"){
+			return "https://img.icons8.com/plasticine/100/000000/twitter.png"
+		}
+		else if (type === "messenger" && self.theme === "handDrawn"){
+			return "https://img.icons8.com/plasticine/100/000000/facebook-messenger.png"
+		}
+		else if (type === "whatsapp" && self.theme === "handDrawn"){
+			return "https://img.icons8.com/plasticine/100/000000/whatsapp.png"
+		}
+		else if (type === "skype" && self.theme === "handDrawn"){
+			return "https://img.icons8.com/plasticine/100/000000/skype.png"
+		}
+		else if (type === "youtube" && self.theme === "pastel"){
+			return "https://img.icons8.com/cotton/64/000000/youtube.png"
+		}
+		else if (type === "instagram" && self.theme === "pastel"){
+			return "https://img.icons8.com/cotton/64/000000/instagram-new.png"
+		}
+		else if (type === "twitter" && self.theme === "pastel"){
+			return "https://img.icons8.com/cotton/64/000000/twitter.png"
+		}
+		else if (type === "messenger" && self.theme === "pastel"){
+			return "https://img.icons8.com/cotton/64/000000/facebook-messenger--v3.png"
+		}
+		else if (type === "whatsapp" && self.theme === "pastel"){
+			return "https://img.icons8.com/cotton/64/000000/whatsapp--v4.png"
+		}
+		else if (type === "skype" && self.theme === "pastel"){
+			return "https://img.icons8.com/cotton/64/000000/skype.png"
+		}
+		else if (type === "youtube" && self.theme === "material"){
+			return "https://img.icons8.com/material-rounded/96/000000/youtube.png"
+		}
+		else if (type === "instagram" && self.theme === "material"){
+			return "https://img.icons8.com/material-rounded/96/000000/instagram-new.png"
+		}
+		else if (type === "twitter" && self.theme === "material"){
+			return "https://img.icons8.com/material-rounded/96/000000/twitter.png"
+		}
+		else if (type === "messenger" && self.theme === "material"){
+			return "https://img.icons8.com/material-rounded/96/000000/facebook-messenger.png"
+		}
+		else if (type === "whatsapp" && self.theme === "material"){
+			return "https://img.icons8.com/material-rounded/96/000000/whatsapp.png"
+		}
+		else if (type === "skype" && self.theme === "material"){
+			return "https://img.icons8.com/material-rounded/96/000000/skype.png"
 		}
 	}
 
