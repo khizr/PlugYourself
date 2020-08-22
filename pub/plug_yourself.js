bba6cf5a496a232a64019c2d0141ccf2f0c155fc
Khizr Khan <khizr.khan@mail.utoronto.ca>
2020-08-21 20:34:02 -0400

    final copy to hand in

--- pub/plug_yourself.js
+++ pub/plug_yourself.js
@@ -4,10 +4,32 @@ console.log('creating js library')
 
 function PlugYourself() {
 	this.bar = this.createBar()
+	this.barID = PlugYourself.counter
+	PlugYourself.counter++
 	this.buttons = []
-	this.theme = "handDrawn"
+	this.theme = "ios"
 	this.iconSize = 20
+	console.log(this)
 	//this.title = this.createTitle()
+}
+
+PlugYourself.counter = 0
+
+PlugYourself.link_variables = {
+	"ios": "ios-glyphs",
+	"color": "color",
+	"gradient": "nolan",
+	"cute_outline": "wired",
+	"cute": "dusk",
+	"dotted": "dotty",
+	"cloud": "clouds",
+	"hand_drawn": "plasticine",
+	"doodle": "doodle",
+	"bubbles": "bubbles",
+	"hand_drawn_outline": "carbon-copy",
+	"office": "office",
+	"clipart": "cute-clipart",
+	"blue": "ultraviolet"
 }
 
 PlugYourself.prototype = {
@@ -15,103 +37,92 @@ PlugYourself.prototype = {
 	createBar: function() {
 		const bar = document.createElement('div')
 		bar.style = 'display: inline-block; padding: 10px; margin: 10px; border: 3px solid;'
-		document.querySelector('body').append(bar)
 		return bar
 	},
 
-	// createTitle: function() {
-	// 	const title_div = document.createElement('div')
-	// 	const title_text = document.createElement('h3')
-	// 	//title_text.appendChild(document.createTextNode("Check out my links below"))
-	// 	title_text.style = "display: inline-block"
-	// 	title_div.appendChild(title_text)
-	// 	this.bar.appendChild(title_div)
-	// 	return title_div
-	// },
+	getBar: function() {
+		return this.bar
+	},
 
 	setColour: function(colour) {
 		this.bar.style.backgroundColor = colour
 	},
 
 	addButton: function(type, link) {
-		var a = document.createElement('a');  
+		var a = document.createElement('a');
         a.href = link;  
 		a.style='color: white; padding: 5px; text-decoration: none; text-transform: uppercase;'
-		
+
 		const image = document.createElement('img')
 		image.src = this.getImageLink(type)
 		image.style.width = self.IconSize + "px"
 		image.style.height = self.IconSize + "px"
+		//image.style.border = "2px solid transparent"
+		//image.style.borderColor = this.bar.style.backgroundColor
+		image.className = "_" + this.barID
 		a.appendChild(image)
-		this.bar.appendChild(a);  
+		this.bar.appendChild(a); 
+		this.buttons.push([a, type, link]) 
 		
 	},
 
+	shapeToVertical: function(){
+		var style = document.createElement('style');
+		var hover_style = document.createTextNode("._" + this.barID + "{display:block;}");
+		style.appendChild(hover_style);
+		
+		var i;
+		for(i=0; i<this.buttons.length; i++){
+			this.buttons[i][0].appendChild(style)
+			console.log(this.buttons[i][0].className)
+		}
+	},
+
+	maxInRow: function(max){
+		var max_width = (this.IconSize + 10) * max + 26
+		this.bar.style.maxWidth = max_width + "px"
+	},
+
+	openInNewTab(){
+		var i
+		for (i = 0; i < this.buttons.length; i++){
+			this.buttons[i][0].target = "_blank"
+		}
+	},
+
+	addHoverDarken: function(){
+		var style = document.createElement('style');
+		var hover_style = document.createTextNode("._" + this.barID + ":hover {opacity: 0.6;}");
+		style.appendChild(hover_style);
+		
+		var i;
+		for(i=0; i<this.buttons.length; i++){
+			this.buttons[i][0].appendChild(style)
+			console.log(this.buttons[i][0].className)
+		}
+	},
+
+	addRoundedCorners: function(){
+		this.bar.style.borderRadius = "10px"
+	},
+
+	setBorderColour: function(colour){
+		this.bar.style.borderColor = colour
+	},
+
 	setIconTheme: function(theme) {
-		self.theme = theme
+		this.theme = theme
 		
 	},
 
 	setIconSize: function(size) {
-		self.IconSize = size
+		this.IconSize = size
 		
 	},
 
-	getImageLink: function(type) {
-		if (type === "youtube" && self.theme === "handDrawn"){
-			return "https://img.icons8.com/plasticine/100/000000/youtube-play.png"
-		}
-		else if (type === "instagram" && self.theme === "handDrawn"){
-			return "https://img.icons8.com/plasticine/100/000000/instagram-new.png"
-		}
-		else if (type === "twitter" && self.theme === "handDrawn"){
-			return "https://img.icons8.com/plasticine/100/000000/twitter.png"
-		}
-		else if (type === "messenger" && self.theme === "handDrawn"){
-			return "https://img.icons8.com/plasticine/100/000000/facebook-messenger.png"
-		}
-		else if (type === "whatsapp" && self.theme === "handDrawn"){
-			return "https://img.icons8.com/plasticine/100/000000/whatsapp.png"
-		}
-		else if (type === "skype" && self.theme === "handDrawn"){
-			return "https://img.icons8.com/plasticine/100/000000/skype.png"
-		}
-		else if (type === "youtube" && self.theme === "pastel"){
-			return "https://img.icons8.com/cotton/64/000000/youtube.png"
-		}
-		else if (type === "instagram" && self.theme === "pastel"){
-			return "https://img.icons8.com/cotton/64/000000/instagram-new.png"
-		}
-		else if (type === "twitter" && self.theme === "pastel"){
-			return "https://img.icons8.com/cotton/64/000000/twitter.png"
-		}
-		else if (type === "messenger" && self.theme === "pastel"){
-			return "https://img.icons8.com/cotton/64/000000/facebook-messenger--v3.png"
-		}
-		else if (type === "whatsapp" && self.theme === "pastel"){
-			return "https://img.icons8.com/cotton/64/000000/whatsapp--v4.png"
-		}
-		else if (type === "skype" && self.theme === "pastel"){
-			return "https://img.icons8.com/cotton/64/000000/skype.png"
-		}
-		else if (type === "youtube" && self.theme === "material"){
-			return "https://img.icons8.com/material-rounded/96/000000/youtube.png"
-		}
-		else if (type === "instagram" && self.theme === "material"){
-			return "https://img.icons8.com/material-rounded/96/000000/instagram-new.png"
-		}
-		else if (type === "twitter" && self.theme === "material"){
-			return "https://img.icons8.com/material-rounded/96/000000/twitter.png"
-		}
-		else if (type === "messenger" && self.theme === "material"){
-			return "https://img.icons8.com/material-rounded/96/000000/facebook-messenger.png"
-		}
-		else if (type === "whatsapp" && self.theme === "material"){
-			return "https://img.icons8.com/material-rounded/96/000000/whatsapp.png"
-		}
-		else if (type === "skype" && self.theme === "material"){
-			return "https://img.icons8.com/material-rounded/96/000000/skype.png"
-		}
+	getImageLink: function(type){
+		var link = "https://img.icons8.com/" + PlugYourself.link_variables[this.theme] + "/" + this.IconSize + "/000000/" + type + ".png"
+		return link
 	}
 
 }
